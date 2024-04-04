48d6ed2c93827812a5243ab766ac29d75e045394
Eduardo Lopez <eduardo.lopez@alebateducation.com>
2024-04-04 09:25:22 +0200

    feat: examples

---
+++ components/ExampleAPIuseFetch.vue
@@ -0,0 +1,19 @@
+<script setup>
+const {
+  data: fetchedPokemon,
+  pending,
+  error,
+} = useFetch("https://pokeapi.co/api/v2/pokemon/1");
+
+const pokemon = computed(() => fetchedPokemon.value);
+
+console.log(pokemon.value);
+</script>
+
+<template>
+  <p v-if="pending">Cargando...</p>
+  <p v-else-if="error">Error al cargar los datos.</p>
+  <section v-if="pokemon">
+    <h2>{{ pokemon.name }}</h2>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <script setup>
-const ExampleAPI = defineAsyncComponent(() =>
+const ExampleAPIFetch = defineAsyncComponent(() =>
   import("@/components/ExampleAPIFetch.vue")
+);
+const ExampleAPIAxios = defineAsyncComponent(() =>
+  import("@/components/ExampleAPIAxios.vue")
+);
+const ExampleAPIuseFetch = defineAsyncComponent(() =>
+  import("@/components/ExampleAPIuseFetch.vue")
 );
 </script>
 
 <template>
   <h1>Ejemplo de las APIs</h1>
-  <ExampleAPI />
+  <!-- <ExampleAPIFetch /> -->
+  <ExampleAPIAxios />
+  <!-- <ExampleAPIuseFetch /> -->
 </template>
